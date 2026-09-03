f6e3aeb3e5a0727e595b1501bd7d2bdcf43c33c5
afeefulhussain <afeefulhussain356@gmail.com>
2026-09-03 07:25:27 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -501,7 +501,8 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     print(f"Starting Hospital & Clinic Management Web Server on port {port}...")
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+
 
